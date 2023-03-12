85d0277cc390019c95ccd913dce08a1b672cdf68
Max Bergman <maxbergman@outlook.com>
2023-03-12 23:51:03 +0100

    1.0.1: Updated Contracts package and added small domain unit test.

---
+++ Domain.UnitTests/ShowNameTests.cs
@@ -0,0 +1,12 @@
+﻿using BingeBot.Domain.Shows;
+
+namespace BingeBot.Domain.UnitTests;
+
+public class ShowNameTests
+{
+	[Fact]
+	public void ShowName_ShouldThrowValidationMessage_WhenNull()
+	{
+		Assert.Throws<ValidationException<NotNullGuard<string>>>(() => new ShowName(value: null!));
+	}
+}
